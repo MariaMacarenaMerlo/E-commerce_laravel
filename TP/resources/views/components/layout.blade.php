65d749c427c3538efa841d463fcfe6423e91d2cc
maca merlo <macarena.merlo@gmail.com>
2025-04-30 19:47:42 -0300

    nav creado pero no es funcional aun

---
+++ TP/resources/views/components/layout.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+    <title>Document</title>
+
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"> Papel Lab </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Quiénes Somos</a>
+                        <a class="nav-link" href="#">Productos</a>
+                        <a class="nav-link" href="#">Contacto</a>
+                        <a class="nav-link" href="#">Blog</a>    
+                    </div>
+                </div>
+                <div>
+                    <a class='nav-link- href='#'>Iniciar Sesión</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer class='conteiner-fluid'>
+        <div class='row'>
+            <div class='col-12'>
+                <a class="nav-link" aria-current="page" href="#">Quiénes Somos</a>
+                <a class="nav-link" href="#">Productos</a>
+                <a class="nav-link" href="#">Contacto</a>
+                <a class="nav-link" href="#">Blog</a> 
+            </div>
+        </div>
+
+
+    </footer>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
